2c7be43b59049d601995fc6d1caa530ce02293b6
sarabastian <seb118@georgetown.edu>
2020-10-27 20:24:46 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,17 @@
 class StudentsController < ApplicationController
+  
+  
   def index
     @students = Student.all
+    render :index
   end
+
+  def show
+    student_id = params[:id]
+    @student = Student.find(student_id)
+  end
+
+
+  
+  
 end
